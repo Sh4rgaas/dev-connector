5a1c91a7fba80310eb5a412d0e3b317c0379ead5
Sh4rgaas <shloosh41@yahoo.com>
2022-02-20 11:51:23 +0200

    Added routing in app.js + link routes on navbar layout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
         <h1>
-            <a href="index.html"><i className="fas fa-code"></i>DevConnector</a>
+          <Link to='/'>
+            <i className="fas fa-code">DevConnector</i>
+          </Link>
         </h1>
         <ul>
-            <li a href="profiles.html">Developers</li>
-            <li a href="register.html">Register</li>
-            <li a href="login.html">Login</li>
+            <li><Link to='!#'>Developers</Link></li>
+            <li><Link to='/register'>Register</Link></li>
+            <li><Link to='/login'>Login</Link></li>
         </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
