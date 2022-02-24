2d327b7aed5123191c92064bab9642a372ee1000
Sh4rgaas <shloosh41@yahoo.com>
2022-02-24 17:30:36 +0200

    Added a create profile form for users with no profile

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -8,7 +8,7 @@ const PrivateRoute = ({
     component: Component,
     auth: { isAuthenticated, loading }
   }) => {
-    // if (loading);
+    if (loading);
     if (isAuthenticated) return <Component />;
   
     return <Navigate to="/login" />;
