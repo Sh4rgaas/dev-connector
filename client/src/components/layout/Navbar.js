19d31ede29e1208f23a0b1f229ec87c8981324b9
Sh4rgaas <shloosh41@yahoo.com>
2022-02-27 14:30:51 +0200

    Added ProfileItem and Profiles components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,6 +7,10 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
   const authLinks = (
         <ul>
+          <li><Link to='/profiles'>
+            Developers
+          </Link>
+          </li>
           <li><Link to='/dashboard'>
           <i className="fas fa-sign-out-alt"></i>{' '}
           <span className='hide-sm'>Dashboard</span></Link></li>
@@ -20,7 +24,7 @@ const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
 
   const guestLinks = (
       <ul>
-            <li><Link to='!#'>Developers</Link></li>
+            <li><Link to='/profiles'>Developers</Link></li>
             <li><Link to='/register'>Register</Link></li>
             <li><Link to='/login'>Login</Link></li>
       </ul>
