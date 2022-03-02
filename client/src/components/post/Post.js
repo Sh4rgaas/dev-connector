739b607a21d1a4450a78286c4dc27c55462df335
Sh4rgaas <shloosh41@yahoo.com>
2022-03-02 16:54:18 +0200

    Remove comment and Add comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
 import { getPost } from '../../actions/post';
 
 
@@ -17,6 +18,7 @@ const Post = ({ getPost, post: { post, loading } }) => {
     <Fragment>
         <Link to='/posts' className='btn'>Back to Posts</Link>
       <PostItem post={post} showActions={false}></PostItem>
+      <CommentForm postId={post._id} />
     </Fragment>
     </section>
 
